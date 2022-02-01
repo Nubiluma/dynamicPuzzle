4602ac6f8c7f15189438dc528445596ebd33522e
Sandra <71071950+Nubiluma@users.noreply.github.com>
2022-02-01 12:52:06 +0100

    added choose and tick method

--- src/main/java/dynamicPuzzle/game/Game.java
+++ src/main/java/dynamicPuzzle/game/Game.java
@@ -1,17 +1,57 @@
 package dynamicPuzzle.game;
 
+import dynamicPuzzle.object.Piece;
+
+import java.util.Scanner;
+
 public class Game {
 
     private Field gameField;
-    private final int size; //for field's size to match game size
+    private final int size; //for field's size to match game size set in Launcher
+    private boolean running;
+    private static Scanner scanner;
 
-    public Game(int size){
+    private Piece choiceA, choiceB, choiceC;
+
+    public Game(int size) {
         this.size = size;
         init();
+        tick();
     }
 
+    //method for setting up the game
     private void init() {
+        running = true;
         gameField = new Field(size);
-        gameField.printField();
+    }
+
+        private void tick() {
+            System.out.println();
+            gameField.printField();
+            choose();
+    }
+
+    public Piece choose() {
+
+        System.out.println("Choose between 1 and 3!");
+        int c = scanner.nextInt();
+
+        if (c == 1 && choiceA != null) {
+            System.out.println("Chosen 1");
+            System.out.println();
+            return choiceA;
+        } else if (c == 2 && choiceB != null) {
+            System.out.println("Chosen 2");
+            System.out.println();
+            return choiceB;
+        } else if (c == 3 && choiceC != null) {
+            System.out.println("Chosen 3");
+            System.out.println();
+            return choiceC;
+        }
+
+        System.out.println("error");
+        return choose();
+
     }
 }

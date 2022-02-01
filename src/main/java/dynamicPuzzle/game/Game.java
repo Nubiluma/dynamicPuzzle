7d30af4b46a6fde8b2b3cf1b9da67bbae85ca1ad
Sandra <71071950+Nubiluma@users.noreply.github.com>
2022-02-01 13:57:11 +0100

    added further methods for gameplay (updateChoices & getRandomID) and integrated java utils Scanner and Random

--- src/main/java/dynamicPuzzle/game/Game.java
+++ src/main/java/dynamicPuzzle/game/Game.java
@@ -2,6 +2,7 @@ package dynamicPuzzle.game;
 
 import dynamicPuzzle.object.Piece;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
@@ -10,6 +11,8 @@ public class Game {
     private final int size; //for field's size to match game size set in Launcher
     private boolean running;
     private static Scanner scanner;
+
+    private final Random random = new Random();
 
     private Piece choiceA, choiceB, choiceC;
 
@@ -22,13 +25,54 @@ public class Game {
     //method for setting up the game
     private void init() {
         running = true;
+        scanner = new Scanner(System.in);
         gameField = new Field(size);
     }
 
-        private void tick() {
+    private void tick() {
+        System.out.println();
+        gameField.printField();
+        updateChoices();
+        choose();
+    }
+
+    public int getRandomID() {
+        return random.nextInt(13); //bound is tied to amount of piece ids (12)
+    }
+
+    public void updateChoices() {
+
+        if (choiceA == null && choiceB == null && choiceC == null) {
+
+            choiceA = new Piece(getRandomID());
+            choiceB = new Piece(getRandomID());
+            choiceC = new Piece(getRandomID());
+        }
+
+        System.out.println("1: ");
+        if (choiceA != null) {
+            choiceA.printPiece(choiceA);
+        } else {
+            System.out.println("N/A");
             System.out.println();
-            gameField.printField();
-            choose();
+        }
+
+        System.out.println("2: ");
+        if (choiceB != null) {
+            choiceB.printPiece(choiceB);
+        } else {
+            System.out.println("N/A");
+            System.out.println();
+        }
+
+        System.out.println("3: ");
+        if (choiceC != null) {
+            choiceC.printPiece(choiceC);
+        } else {
+            System.out.println("N/A");
+            System.out.println();
+        }
+
     }
 
     public Piece choose() {

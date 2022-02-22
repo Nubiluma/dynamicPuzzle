12070f2832f116b5520d5df52c16a7c5fe1b566e
Sandra <71071950+Nubiluma@users.noreply.github.com>
2022-02-22 11:19:42 +0100

    edited chooseGameSize() to limit game size preference

--- src/main/java/dynamicPuzzle/game/Launcher.java
+++ src/main/java/dynamicPuzzle/game/Launcher.java
@@ -7,6 +7,7 @@ public class Launcher {
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_RED = "\u001B[31m";
 
     public static void main(String[] args) {
 
@@ -20,14 +21,22 @@ public class Launcher {
      */
     public static int chooseGameSize() {
 
+        int minSize = 5;
+        int maxSize = 11;
         Scanner scanner = new Scanner(System.in);
         System.out.println(ANSI_CYAN);
         System.out.println("Enter preferred size for the game.");
-        System.out.println("Input equals amount of rows and columns. A minimum size of 5 is recommended.");
+        System.out.println("Input equals amount of rows and columns. A minimum size of " + minSize +
+                " and a maximum of size of " + maxSize +" will be accepted.");
         System.out.println(ANSI_WHITE + "(For all inputs press enter to confirm.)" + ANSI_RESET);
         System.out.print(ANSI_CYAN + "Preferred size: " + ANSI_RESET);
 
-        return scanner.nextInt();
+        int size = scanner.nextInt();
+        if (size >= minSize && size <= maxSize){
+            return size;
+        }
+        System.out.println(ANSI_RED + "Input invalid. Enter a number between " + minSize + " and " + maxSize + "!" + ANSI_RESET);
+        return chooseGameSize();
     }
 
 }
